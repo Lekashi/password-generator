529da1ea5bcc229abad9a11ae27f27860c1b81d1
Nick Pisano <pisano.nicholas@yahoo.com>
2022-05-30 14:09:15 -0700

    Halfway done with javascript pseudocode

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,36 +1,54 @@
-//  Create variables to hold the character options for Special, lowercase, uppercas and numeric character
+//  Create variables to hold the character options for Special, lowercase, uppercase and numeric character
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var SpecialCase = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":","'",'"'];
+var numericalNumeral = ["1","2","3","4","5","6","7","8","9"]
 
 // create an empty array that will hold the user selected characters
+var OptionsArray = [];
 
 // var possibleChars = [];
+var possibleChars = [];
+
 
 // on game start prompt for users numbers of character and hold in variable
-// var numChars = prompt("How many character ")
 
-// check the users input and confirm they put in a number between 8 and 128
+function PasswordFinder() {
+    // var numChars = prompt("How many character ")
+    var numChars = prompt("How many characters would you like? Between 8 and 128")
+    
+    // check the users input and confirm they put in a number between 8 and 128
+    if (numChars  < 8 || numChars > 128) {
+        alert("Too many chars or not enough chars. please select a number between 8 and 128")
+        return PasswordFinder;
+    }
+    // write function that validates users input
+    else if
+     (typeof(numC))
 
-// write function that validates users input
 
-// functions validateUserInput(chars) {
-// if (chars < 8 || chars > 128) {
-//      return null  
-// }
-// }
-
-
-// after validation create variables for character selections
-
-// var specialChars= confirm("Would you link special characters")
-// var numericalNums= confirm("Would you link special characters")
-// var lowerChars= confirm("Would you link special characters")
-// var upperChars= confirm("Would you link special characters")
-
-// validate that at least one option was selected
-
-// if (!specialChars && !numericalNums && !lowerChars && !upperChars) {
-//      alert ("You need to select at least one type of character");
-// return gerneratePassword()
-// }
+    
+    // functions validateUserInput(chars) {
+        // if (chars < 8 || chars > 128) {
+            //      return null  
+            // }
+            // }
+            
+            
+            // after validation create variables for character selections
+            
+            // var specialChars= confirm("Would you link special characters")
+            // var numericalNums= confirm("Would you link special characters")
+            // var lowerChars= confirm("Would you link special characters")
+            // var upperChars= confirm("Would you link special characters")
+            
+            // validate that at least one option was selected
+            
+            // if (!specialChars && !numericalNums && !lowerChars && !upperChars) {
+                //      alert ("You need to select at least one type of character");
+                // return gerneratePassword()
+                // }
+            }
 
 // after validation is complete start pushiong the selected characters into possible chars array
 
